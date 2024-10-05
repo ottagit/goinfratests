1799b3f7db6fa095f2dc08cfd35c494c5c61ad01
ottagit <ottaosawa@gmail.com>
2024-10-05 10:45:19 +0300

    add unit tests for alb and hello world modules

---
+++ hello_gophers_app_example_test.go
@@ -0,0 +1,46 @@
+package test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestHelloGophersAppExample(t *testing.T) {
+	opts := &terraform.Options{
+		TerraformDir: "../examples/hello-gophers-app/loneranger",
+
+		Vars: map[string]interface{}{
+			"mysql_config": map[string]interface{}{
+				"address": "mock-value",
+				"port":    90123,
+			},
+		},
+	}
+
+	// Clean up everything after the test
+	defer terraform.Destroy(t, opts)
+	terraform.InitAndApply(t, opts)
+
+	albDnsName := terraform.OutputRequired(t, opts, "alb_dns_name")
+	url := fmt.Sprintf("http://%s", albDnsName)
+
+	maxRetries := 10
+	timeBetweenRetries := 10 * time.Second
+
+	http_helper.HttpGetWithRetryWithCustomValidation(
+		t,
+		url,
+		nil,
+		maxRetries,
+		timeBetweenRetries,
+		func(status int, body string) bool {
+			return status == 200 && strings.Contains(body, "Hello, Gophers!")
+		},
+	)
+}
